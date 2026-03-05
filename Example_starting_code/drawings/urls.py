647f45a54eac8603887a6989d2071ba7057c94a7
gianniskardasis1-byte <gianniskardasis1@gmail.com>
2026-03-05 10:54:00 +0200

    added example version for sketchit

--- Example_starting_code/drawings/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<slug:room_id>', views.drawing_room, name='drawing_room')
-]
--- Example_starting_code/words_lists/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WordsListsConfig(AppConfig):
-    name = 'words_lists'
