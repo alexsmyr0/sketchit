e73c835cb8e1adcef8de8645018214e19e9232de
alexsmyro <alexsmyro@gmail.com>
2026-03-08 23:47:38 +0200

    Registered apps into INSTALED_APPS in django config

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig',
+    'rooms.apps.RoomsConfig',
+    'games.apps.GamesConfig',
+    'words.apps.WordsConfig',
 ]
 
 MIDDLEWARE = [
