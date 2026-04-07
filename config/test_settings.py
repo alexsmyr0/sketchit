1a703bec7ff1a8d6dda58881af3758f7079431a9
alexsmyr0 <alex.smyroglou@mc-class.gr>
2026-04-07 20:09:42 +0300

    Fixed Docker MySQL test configuration and documented test commands

--- config/test_settings.py
+++ config/test_settings.py
@@ -1,9 +1,10 @@
 import os
 
-os.environ.setdefault("MYSQL_DATABASE", "test_db")
-os.environ.setdefault("MYSQL_USER", "test_user")
-os.environ.setdefault("MYSQL_PASSWORD", "test_password")
-os.environ.setdefault("REDIS_URL", "redis://127.0.0.1:6379/1")
+os.environ["MYSQL_DATABASE"] = "sketchit"
+os.environ["MYSQL_USER"] = "root"
+os.environ["MYSQL_PASSWORD"] = "sketchit-root"
+os.environ["MYSQL_HOST"] = "mysql"
+os.environ["REDIS_URL"] = "redis://redis:6379/1"
 
 from .settings import *
 
