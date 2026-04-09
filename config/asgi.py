04ba9374ceb85014ed440ec07dfc782bf0accdc1
alexsmyr0 <alex.smyroglou@mc-class.gr>
2026-04-09 22:27:45 +0300

    Fixed Django ASGI static file serving for entry page assets

--- config/asgi.py
+++ config/asgi.py
@@ -5,10 +5,14 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
+from django.conf import settings
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 from django.core.asgi import get_asgi_application
 
 # Initialize Django before importing anything that may touch models.
 django_asgi_app = get_asgi_application()
+if settings.DEBUG:
+    django_asgi_app = ASGIStaticFilesHandler(django_asgi_app)
 
 from config.routing import websocket_urlpatterns
 
