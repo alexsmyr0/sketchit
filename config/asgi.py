3e343e9895a61f930601970a651446ec3e6e9370
alexsmyr0 <alex.smyroglou@mc-class.gr>
2026-04-09 22:23:29 +0300

    Fixed ASGI startup by initializing Django settings before importing websocket routing

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,16 @@
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from config.routing import websocket_urlpatterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
 # Initialize Django before importing anything that may touch models.
 django_asgi_app = get_asgi_application()
+
+from config.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
