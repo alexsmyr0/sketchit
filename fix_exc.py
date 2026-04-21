1a76ecde9f4861c4f440652c72a1d25be9169ae1
nikolaschristidis <nikolas.christidis.2006@gmail.com>
2026-04-22 00:07:48 +0300

    Cleanup temporary test stabilization scripts

--- fix_exc.py
+++
@@ -1,6 +0,0 @@
-import re
-with open('rooms/consumers.py', 'r') as f:
-    c = f.read()
-c = c.replace('except Exception as e:', 'except BaseException as e:\n            import traceback\n            print("CAUGHT EXCEPTION IN CONSUMER:")\n            traceback.print_exc()')
-with open('rooms/consumers.py', 'w') as f:
-    f.write(c)
